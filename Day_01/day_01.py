270a5ca6aeb29c72cdd48044f9d93cf2c4908b67
daniela <ikonomovad@gmail.com>
2023-12-01 15:47:46 +0100

    Day 1 - Part 2 done.

--- Day_01/day_01.py
+++ Day_01/day_01.py
@@ -1,3 +1,5 @@
+# part 1
+
 values = []
         
 with open('input.txt') as file:
@@ -13,3 +15,45 @@ for value in values:
     sum += int(value)
     
 print(sum)
+
+#part 2
+
+stringNumbers = {
+    'one': '1', 
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+formattedLines = []
+
+with open('input.txt') as file:
+    for line in file:
+        integer = ''
+        for ind, char in enumerate(line):
+            subString = ''
+            if char.isdigit():
+                integer += char
+            else:
+                subString = line[ind:]
+                for numAsString, num in stringNumbers.items():
+                    if subString.startswith(numAsString):
+                        integer += num
+        formattedLines.append(integer)
+ 
+values2 = []   
+
+for number in formattedLines:
+    newLine = number[0] + number[-1]
+    values2.append(newLine)
+
+sum2 = 0    
+for value in values2:
+    sum2 += int(value)
+    
+print(sum2)
