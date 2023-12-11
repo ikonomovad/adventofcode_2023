c6c7dce383b639bfcd48b2d0f99e545c2d1d3464
daniela <ikonomovad@gmail.com>
2023-12-11 21:50:49 +0100

    Day 11 - Part 1 and 2 Done.

---
+++ Day_11/day_11_p2.py
@@ -0,0 +1,40 @@
+from bisect import bisect
+
+with open('input.txt') as file:
+    grid = file.read().split('\n')
+
+    cols_to_expand = []
+    rows_to_expand = []
+
+    for i in range(len(grid[0])):
+        column = [grid[j][i] for j in range(len(grid))]
+
+        if column.count('.') == len(column):
+            cols_to_expand.append(i)
+
+    for i, row in enumerate(grid):
+        if row.count('.') == len(row):
+            rows_to_expand.append(i)
+
+    galaxy_map = []
+    expand = 999999  # -1 for the col/row we are replacing
+    for i, row in enumerate(grid):
+        for j, char in enumerate(row):
+            if char == '#':
+                galaxy_map.append((i + expand*(bisect(rows_to_expand, i)),
+                                   j + expand*(bisect(cols_to_expand, j))))
+
+    pairs = []
+    # finding existing pairs
+    for i in range(len(galaxy_map)):
+        for j in range(i + 1, len(galaxy_map)):
+            pairs.append([galaxy_map[i], galaxy_map[j]])
+
+    steps_between = []
+    for pair in pairs:
+        i = pair[0]
+        j = pair[1]
+
+        steps_between.append(abs(i[0] - j[0]) + abs(i[1] - j[1]))
+
+    print(sum(steps_between))
